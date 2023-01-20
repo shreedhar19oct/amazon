ab73bf7dcee3243df9c54e1306ea132dbfe5fd5f
shreedhar19oct <shreedhar19oct@gmail.com>
2023-01-20 10:49:42 +0530

    data generated dynamically

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import react, {Component, component} from 'react';
 import classes from './App.module.css'
 import ProductPreview from './ProductPreview';
 import ProductDetails from './productDetails';
+import ProductData from './ProductData';
 
-function App() {
+class App extends Component {
+
+  state = {
+    productData: ProductData
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -17,12 +25,13 @@ function App() {
         </div>
 
         <div className={classes.ProductData}>
-          <ProductDetails />
+          <ProductDetails data={this.state.productData}/>
         </div>
 
       </div>
     </div>
   );
+}    
 }
 
 export default App;
--- src/productDetails.js
+++ src/productDetails.js
@@ -1,23 +1,37 @@
 import React from "react";
 import classes from './ProductDetails.module.css';
-import ProductData from "./ProductData";
 
-const ProductDetails =(props) => {
+const ProductDetails = (props) => {
+    const colorOptions = props.data.colorOptions.map((item,pos) => {
+        const classArr = [classes.ProductImage]
+        if(pos === 0){
+          classArr.push(classes.SelectedProductImage);
+        }
+        return(
+            <img key={pos} className={classArr.join(' ')} src={item.imageUrl} alt={item.styleName} />
+        );
+    })
+
+    const featureList = props.data.featureList.map((item,pos) => {
+      const classArr = [classes.FeatureItem]
+      if(pos === 0){
+        classArr.push(classes.SelectedFeatureItem);
+      }
+      return(
+      <button key={pos} className={classArr.join(' ')}>{item}</button>
+      )
+    })
     return(
         <div className={classes.ProductData}>
-        <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
-        <p className={classes.ProductDescription}>{ProductData.description}</p>
+        <h1 className={classes.ProductTitle}>{props.data.title}</h1>
+        <p className={classes.ProductDescription}>{props.data.description}</p>
         <h3 className={classes.Sectionheading}>Select Color</h3>
         <div>
-          <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red colored watch" />
-          <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue colored watch" />
-          <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple colored watch" />
-          <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src="https://imgur.com/iOeUBV7.png" alt="Black colored watch" />
+        {colorOptions}
         </div>
         <h3 className={classes.Sectionheading}>Features</h3>
         <div>
-          <button className={[classes.FeatureItem, classes.SelectedFeatureItem].join(' ')}>Time</button>
-          <button className={classes.FeatureItem}>Heart Rate</button>
+          {featureList}
         </div>
         <button className={classes.PrimaryButton}>Buy Now</button>
       </div>
